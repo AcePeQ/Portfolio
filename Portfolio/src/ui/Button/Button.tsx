4d09da0f4ccce8f4d96918432c914ecb2ce8b274
AcePeQ <maciej.nojszewski22@gmail.com>
2025-01-16 01:07:26 +0100

    feat: create Button component and add button component to hero component with redirect to about me

---
+++ Portfolio/src/ui/Button/Button.tsx
@@ -0,0 +1,20 @@
+import { MouseEventHandler } from "react";
+import styles from "./Button.module.css";
+
+function Button({
+  children,
+  type,
+  onClick,
+}: {
+  children: string;
+  type: string;
+  onClick: MouseEventHandler;
+}) {
+  return (
+    <button onClick={onClick} className={`${styles.button} ${styles[type]}`}>
+      {children}
+    </button>
+  );
+}
+
+export default Button;
--- Portfolio/src/ui/Hero/Hero.tsx
+++ Portfolio/src/ui/Hero/Hero.tsx
@@ -1,15 +1,29 @@
+import { useNavigate } from "react-router";
+import Button from "../Button/Button";
 import styles from "./Hero.module.css";
 
 function Hero() {
+  const navigate = useNavigate();
+
+  function handleRedirectTo() {
+    navigate("/aboutme");
+  }
+
   return (
     <div className={styles.hero_wrapper}>
       <p className={styles.greeting}>Hi, I'm</p>
       <p className={styles.name}>Maciej Nojszewski</p>
-      <p className={styles.profession}>I'm a Front-End Developer</p>
+      <p className={styles.profession}>
+        I'm a <span className={styles.highlight}>Front-End Developer</span>
+      </p>
       <p className={styles.subtext}>
         I'm currently focused on expending my experince designing and developing
         modern websites
       </p>
+
+      <Button onClick={handleRedirectTo} type="primary">
+        About me
+      </Button>
     </div>
   );
 }
