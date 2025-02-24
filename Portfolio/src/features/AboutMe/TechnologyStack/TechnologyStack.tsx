fa517ec2c27d1f99d686722aca025ecdc794c83b
AcePeQ <maciej.nojszewski22@gmail.com>
2025-02-24 16:12:12 +0100

    feat: add tools i've used and create glass effect to technology item background

--- Portfolio/src/features/AboutMe/TechnologyStack/TechnologyStack.tsx
+++ Portfolio/src/features/AboutMe/TechnologyStack/TechnologyStack.tsx
@@ -7,13 +7,24 @@ const technologiesUsed = [
   { label: "JavaScript", src: "js.svg" },
   { label: "React", src: "react.svg" },
   { label: "TypeScript", src: "typescript.svg" },
-  { label: "C-sharp", src: "c-sharp.svg" },
+  { label: "Bootstrap", src: "bootstrap.svg" },
+  { label: "C#", src: "c-sharp.svg" },
   { label: "MySql", src: "mysql.svg" },
 ];
 
 const technologiesPlanToLearn = [
   { label: "Node JS", src: "nodejs-2.svg" },
+  { label: "Next JS", src: "nextjs.png" },
   { label: "Mango DB", src: "mangodb.svg" },
+  { label: "Tailwind CSS", src: "tailwind.svg" },
+];
+
+const toolsUsed = [
+  { label: "VS Code", src: "vscode.svg" },
+  { label: "Visual Studio", src: "vstudio.svg" },
+  { label: "Figma", src: "figma.svg" },
+  { label: "Github", src: "github.svg" },
+  { label: "Git", src: "git.svg" },
 ];
 
 function TechnologyStack() {
@@ -23,6 +34,15 @@ function TechnologyStack() {
         <h2 className={styles.technology_title}>Technologies I've used</h2>
         <ul className={styles.technologies_list}>
           {technologiesUsed.map((technology) => (
+            <TechnologyItem technology={technology} />
+          ))}
+        </ul>
+      </div>
+
+      <div className={styles.technology_used}>
+        <h2 className={styles.technology_title}>Tools I've used</h2>
+        <ul className={styles.technologies_list}>
+          {toolsUsed.map((technology) => (
             <TechnologyItem technology={technology} />
           ))}
         </ul>
